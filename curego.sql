6a074ac6bb28fd7fd6ee771e250ba6f7eeb98f2c
Jana18kh <139820709+Jana18kh@users.noreply.github.com>
2025-04-06 19:16:11 +0300

    Add files via upload

---
+++ curego.sql
@@ -0,0 +1,279 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Apr 06, 2025 at 04:15 PM
+-- Server version: 8.0.40
+-- PHP Version: 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `curego`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Appointment`
+--
+
+CREATE TABLE `Appointment` (
+  `id` int NOT NULL,
+  `PatientID` int NOT NULL,
+  `DoctorID` int NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `reason` text COLLATE utf8mb4_general_ci NOT NULL,
+  `status` enum('Pending','Confirmed','Done') COLLATE utf8mb4_general_ci DEFAULT 'Pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Appointment`
+--
+
+INSERT INTO `Appointment` (`id`, `PatientID`, `DoctorID`, `date`, `time`, `reason`, `status`) VALUES
+(1, 1, 1, '2025-06-15', '10:00:00', 'Heart Checkup', 'Pending'),
+(2, 2, 2, '2025-07-20', '14:00:00', 'Headache', 'Confirmed'),
+(3, 3, 3, '2025-08-25', '11:00:00', 'Routine Checkup', 'Done');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Doctor`
+--
+
+CREATE TABLE `Doctor` (
+  `id` int NOT NULL,
+  `firstName` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `lastName` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `uniqueFileName` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `SpecialityID` int NOT NULL,
+  `emailAddress` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Doctor`
+--
+
+INSERT INTO `Doctor` (`id`, `firstName`, `lastName`, `uniqueFileName`, `SpecialityID`, `emailAddress`, `password`) VALUES
+(1, 'Mona', 'Ali', 'dr. Mona_Ali.png', 1, 'dr.mona@example.com', '$2a$12$A/szkGeuFokxzmulX3i75OsIZj8mHmb/Err06sIFK633Oh/TBfeQu'),
+(2, 'Khalid', 'Jamal', 'dr.khalid_jamal.png', 2, 'dr.khalid@example.com', '$2a$12$A/szkGeuFokxzmulX3i75OsIZj8mHmb/Err06sIFK633Oh/TBfeQu'),
+(3, 'Fatima', 'Nasser', 'dr.fatima_nasser.png', 3, 'dr.fatima@example.com', '$2a$12$A/szkGeuFokxzmulX3i75OsIZj8mHmb/Err06sIFK633Oh/TBfeQu');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Medication`
+--
+
+CREATE TABLE `Medication` (
+  `id` int NOT NULL,
+  `MedicationName` varchar(100) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Medication`
+--
+
+INSERT INTO `Medication` (`id`, `MedicationName`) VALUES
+(1, 'Aspirin'),
+(2, 'Ibuprofen'),
+(3, 'Paracetamol');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Patient`
+--
+
+CREATE TABLE `Patient` (
+  `id` int NOT NULL,
+  `firstName` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `lastName` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `gender` enum('Male','Female') COLLATE utf8mb4_general_ci NOT NULL,
+  `DoB` date NOT NULL,
+  `emailAddress` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Patient`
+--
+
+INSERT INTO `Patient` (`id`, `firstName`, `lastName`, `gender`, `DoB`, `emailAddress`, `password`) VALUES
+(1, 'Saleh', 'Ahmad', 'Male', '1990-05-15', 'saleh@example.com', '$2a$12$A/szkGeuFokxzmulX3i75OsIZj8mHmb/Err06sIFK633Oh/TBfeQu'),
+(2, 'Omar', 'Ibrahim', 'Male', '1985-08-22', 'omar@example.com', '$2a$12$A/szkGeuFokxzmulX3i75OsIZj8mHmb/Err06sIFK633Oh/TBfeQu'),
+(3, 'Khalid', 'Nader', 'Male', '2000-01-30', 'Khalid@example.com', '$2a$12$A/szkGeuFokxzmulX3i75OsIZj8mHmb/Err06sIFK633Oh/TBfeQu');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Prescription`
+--
+
+CREATE TABLE `Prescription` (
+  `id` int NOT NULL,
+  `AppointmentID` int NOT NULL,
+  `MedicationID` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Prescription`
+--
+
+INSERT INTO `Prescription` (`id`, `AppointmentID`, `MedicationID`) VALUES
+(1, 1, 1),
+(2, 2, 2),
+(3, 3, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Speciality`
+--
+
+CREATE TABLE `Speciality` (
+  `id` int NOT NULL,
+  `speciality` varchar(100) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Speciality`
+--
+
+INSERT INTO `Speciality` (`id`, `speciality`) VALUES
+(1, 'Cardiology'),
+(2, 'Neurology'),
+(3, 'Pediatrics'),
+(4, 'Orthopedics'),
+(5, 'Dermatology'),
+(6, 'Psychiatry'),
+(7, 'Oncology'),
+(8, 'Gynecology');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Appointment`
+--
+ALTER TABLE `Appointment`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `PatientID` (`PatientID`),
+  ADD KEY `DoctorID` (`DoctorID`);
+
+--
+-- Indexes for table `Doctor`
+--
+ALTER TABLE `Doctor`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `emailAddress` (`emailAddress`),
+  ADD KEY `SpecialityID` (`SpecialityID`);
+
+--
+-- Indexes for table `Medication`
+--
+ALTER TABLE `Medication`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Patient`
+--
+ALTER TABLE `Patient`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `emailAddress` (`emailAddress`);
+
+--
+-- Indexes for table `Prescription`
+--
+ALTER TABLE `Prescription`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `AppointmentID` (`AppointmentID`),
+  ADD KEY `MedicationID` (`MedicationID`);
+
+--
+-- Indexes for table `Speciality`
+--
+ALTER TABLE `Speciality`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Appointment`
+--
+ALTER TABLE `Appointment`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `Doctor`
+--
+ALTER TABLE `Doctor`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `Medication`
+--
+ALTER TABLE `Medication`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `Patient`
+--
+ALTER TABLE `Patient`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `Prescription`
+--
+ALTER TABLE `Prescription`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `Speciality`
+--
+ALTER TABLE `Speciality`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Appointment`
+--
+ALTER TABLE `Appointment`
+  ADD CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`PatientID`) REFERENCES `Patient` (`id`),
+  ADD CONSTRAINT `appointment_ibfk_2` FOREIGN KEY (`DoctorID`) REFERENCES `Doctor` (`id`);
+
+--
+-- Constraints for table `Doctor`
+--
+ALTER TABLE `Doctor`
+  ADD CONSTRAINT `doctor_ibfk_1` FOREIGN KEY (`SpecialityID`) REFERENCES `Speciality` (`id`);
+
+--
+-- Constraints for table `Prescription`
+--
+ALTER TABLE `Prescription`
+  ADD CONSTRAINT `prescription_ibfk_1` FOREIGN KEY (`AppointmentID`) REFERENCES `Appointment` (`id`),
+  ADD CONSTRAINT `prescription_ibfk_2` FOREIGN KEY (`MedicationID`) REFERENCES `Medication` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
