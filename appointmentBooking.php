5401edab52cd4ad863e5c2165d4d5c5269bfe305
Reema204 <reema8968@gmail.com>
2025-04-23 23:26:08 +0300

    Update appointmentBooking.php

--- appointmentBooking.php
+++ appointmentBooking.php
@@ -8,10 +8,10 @@ if (!isset($_SESSION['user_id'])) {
 }
 
 // Database connection
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "curego";
+$servername = "sql110.infinityfree.com";
+$username = "if0_38818376";
+$password = "VZrJJdHAmkIV";
+$dbname = "if0_38818376_curego";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
